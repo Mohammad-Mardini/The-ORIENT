ccd1d896d0bd23d24956d386ed30d6bfc2173cd0
Mohammad-Mardini <mardini_mohammad@hotmail.com>
2022-01-26 09:58:59 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+from glob import glob
+import re, sys
+
+with open('orient/_version.py', 'r') as f:
+    __version__ = re.match(r"""__version__ = ["'](.*?)['"]""", f.read()).group(1)
+
+
+libraries = ['gsl', 'gslcblas']
+if sys.platform != 'darwin': libraries.append('stdc++fs')
+
+ext_modules = [
+    Pybind11Extension(
+        'orient.orient',
+        glob('orient/src/*.cpp'),
+        include_dirs=['orient/src'],
+        cxx_std=17,
+        libraries=libraries
+    ),
+]
+
+setup(
+    name='orient',
+    version=__version__,
+    ext_modules=ext_modules,
+    packages=['orient'],
+    package_data={
+        '': ['data/*.dat']
+    }
+)
