e057fb745073e0da50440efc15be89a793ad5f97
Yohai Meiron <yohai@meiron.org>
2022-09-27 17:56:35 -0400

    Added the Orbit convenience class

--- orient/__init__.py
+++ orient/__init__.py
@@ -118,3 +118,95 @@ class Integrate:
     @property
     def E(self):
         raise NotImplementedError
+
+from typing import Optional
+import scipy.interpolate
+class Orbit:
+    def __init__(self,
+                 galaxy:      Galaxy,
+                 ic:          list,
+                 t_min:       np.double,
+                 t_max:       np.double,
+                 stride_size: Optional[np.double] = None,
+                 max_size:    int = 100000,
+                 kms:         bool = True,
+                 icrs:        bool = False):
+        t = None
+        t_apsis = None
+        if stride_size is None:
+            stride_size = (t_max - t_min)/10000
+            sign = np.sign(stride_size)
+            stride_size = sign * 2**(np.floor(np.log(abs(stride_size))/np.log(2)))
+        self.integrate_obj = Integrate(galaxy, ic, t_min, t_max, stride_size, max_size, kms, icrs)
+        data = self.integrate_obj._data
+        self._t = np.arange(t_min, t_max, stride_size)
+        if t_min > t_max:
+            self._t = self._t[::-1]
+            data = data[::-1]
+        self.data_interp = scipy.interpolate.interp1d(self._t, data, axis=0)
+
+        r_vec = data[:,:3]
+        x, y, z  = r_vec.T
+        v_vec = data[:,3:]
+        vx, vy, vz = v_vec.T
+        
+        r = np.linalg.norm(r_vec, axis=1)
+        t = self._t
+
+        sign_diff = np.diff(np.sign(np.diff(r)))
+        peri_mask = sign_diff ==  2
+        apo_mask  = sign_diff == -2
+        self._t_peri = t[1:-1][peri_mask]
+        self._r_peri = r[1:-1][peri_mask]
+        self._t_apo  = t[1:-1][apo_mask]
+        self._r_apo  = r[1:-1][apo_mask]
+
+        zmax_mask = np.zeros(len(t), dtype=bool)
+        params = np.array(galaxy.get_fit_params(t))
+        galaxy_phi   = params[:,0]
+        galaxy_theta = params[:,1]
+        galaxy_direction = np.array([np.cos(galaxy_phi)*np.sin(galaxy_theta),
+                                     np.sin(galaxy_phi)*np.sin(galaxy_theta),
+                                     np.cos(galaxy_theta)]).T
+        z_transformed = np.sum((r_vec * galaxy_direction[None,:])[0], axis=1)
+        zmax_mask[1:-1] = np.diff(np.sign(np.diff(z_transformed)))!=0
+        self._zmax = np.abs(z_transformed[zmax_mask])
+        self._t_zmax = t[zmax_mask]
+
+        t_apsis = np.empty((len(self._t_peri) + len(self._t_apo)))
+        r_apsis = np.empty_like(t_apsis)
+        if self._t_peri[0] < self._t_apo[0]:
+            t_apsis[::2] = self._t_peri
+            r_apsis[::2] = self._r_peri
+            t_apsis[1::2] = self._t_apo
+            r_apsis[1::2] = self._r_apo
+        else:
+            t_apsis[::2] = self._t_apo
+            r_apsis[::2] = self._r_apo
+            t_apsis[1::2] = self._t_peri
+            r_apsis[1::2] = self._r_peri
+
+        a = .5*(r_apsis[:-1] + r_apsis[1:])
+        e = np.abs(r_apsis[:-1] - r_apsis[1:])/(2*a)
+            
+        self.e = scipy.interpolate.interp1d(.5*(t_apsis[:-1] + t_apsis[1:]), e, bounds_error=False)
+        self.zmax = scipy.interpolate.interp1d(self._t_zmax, self._zmax, bounds_error=False)
+        self.r_apo = scipy.interpolate.interp1d(self._t_apo, self._r_apo, bounds_error=False)
+
+    @property
+    def x(self):  return self.integrate_obj._data[:,0]
+    
+    @property
+    def y(self):  return self.integrate_obj._data[:,1]
+    
+    @property
+    def z(self):  return self.integrate_obj._data[:,2]
+    
+    @property
+    def vx(self): return self.integrate_obj._data[:,3]
+    
+    @property
+    def vy(self): return self.integrate_obj._data[:,4]
+    
+    @property
+    def vz(self): return self.integrate_obj._data[:,5]
